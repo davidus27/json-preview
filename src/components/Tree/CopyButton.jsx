aa5bc2ff9e0f2bd1b04b4ca216f0ed5e22d1a26d
davidus27 <davidisbest27@gmail.com>
2025-04-28 01:17:38 +0200

    remove unused references

--- src/components/Tree/CopyButton.jsx
+++ src/components/Tree/CopyButton.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { showToast } from '../ToastContainer';
 
 export default function CopyButton({ textToCopy, visible }) {
